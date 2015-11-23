590b6fee03571f2a7938d5b2decfdac4a360fecf
Daniele Santoro <psyacca@gmail.com>
2015-11-23 11:04:38 +0100

    preparation for heroku deploy

--- src/introsde/dsantoro/a2/MyApplicationConfig.java
+++ src/introsde/dsantoro/a2/MyApplicationConfig.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("sdelab")
+@ApplicationPath("dsantoroa2")
 public class MyApplicationConfig extends ResourceConfig {
     public MyApplicationConfig () {
         packages("introsde.dsantoro.a2.resources");
